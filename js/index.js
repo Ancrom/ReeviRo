59ef237c3e1ee4da1d88efac44f159d2c14666f2
Roman Antsupov <remon90@list.ru>
2023-04-13 04:19:37 +0300

    animation fade up

--- js/index.js
+++ js/index.js
@@ -8,17 +8,17 @@ function lightbox() {
   const lightbox = document.querySelector(".lightbox");
   const galleryItems = document.querySelectorAll(".gallery__item");
 
-  for (let i = 0; i < galleryItems.length; i++) {
-    galleryItems[i].onclick = () => {
+  galleryItems.forEach((item) => {
+    item.onclick = () => {
       // getting path to fullsize clicked image
-      const src = galleryItems[i].children[0].getAttribute("src").split("/");
+      const src = item.children[0].getAttribute("src").split("/");
       src.splice(src.indexOf("preview"), 1);
       const newPath = src.join("/");
       // lightbox show
       lightbox.children[0].setAttribute("src", newPath);
       lightbox.classList.add("active");
     };
-  }
+  });
 
   // lightbox close
   lightbox.onclick = () => {
