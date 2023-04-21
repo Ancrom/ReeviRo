3ce94fe07121ccd788a367eefdcb725171973c31
Roman Antsupov <remon90@list.ru>
2023-04-21 10:37:02 +0300

    project?

--- js/index.js
+++ js/index.js
@@ -1,22 +1,24 @@
 /* ==================== BASE ==================== */
 
-const galleryItems = document.querySelectorAll(".gallery__item");
+const galleryItems = document.querySelectorAll(".gallery__item"); // using in changeAOSEffect and lightbox
 AOS.init(); // animate on scroll initialize
 changeAOSEffect(); // change AOS effect for some blocks depending on resolution
 window.onresize = () => changeAOSEffect();
 burger();
 lightbox();
+accordion();
 
 /* ==================== CHANGE AOS EFFECT ==================== */
 
 function changeAOSEffect() {
   const nav = document.querySelector(".nav");
   if (window.innerWidth < 1026) nav.dataset.aos = "";
-  if (window.innerWidth < 769)
+  if (window.innerWidth < 769) {
     galleryItems.forEach((item) => {
       item.dataset.aos = "fade-up";
-      item.dataset.aosDelay = "";
+      delete item.dataset.aosDelay;
     });
+  }
 }
 
 /* ==================== BURGER ==================== */
@@ -36,23 +38,47 @@ function burger() {
 
 function lightbox() {
   const lightbox = document.querySelector(".lightbox");
-  galleryItems.forEach((item) => {
-    item.onclick = () => {
-      // changing scr to fullsize image
-      const src = item.children[0].getAttribute("src").split("/");
-      if (src.includes("preview")) src.splice(src.indexOf("preview"), 1);
-      lightbox.children[0].setAttribute("src", src.join("/"));
-      // lightbox show
-      lightbox.classList.add("active");
-      if (window.innerWidth < 768) document.body.classList.add("no-scroll");
-    };
-  });
+  const accordionImgs = document.querySelectorAll(".accordion__img");
+  // lightbox show
+  const lightboxShow = (item) => {
+    // changing scr to fullsize image
+    const src = item.children[0].getAttribute("src").split("/");
+    if (src.includes("preview")) src.splice(src.indexOf("preview"), 1);
+    lightbox.children[0].setAttribute("src", src.join("/"));
+    lightbox.classList.add("active");
+    if (window.innerWidth < 768) document.body.classList.add("no-scroll");
+  };
   // lightbox close
   const lightBoxClose = () => {
     document.body.classList.remove("no-scroll");
     lightbox.classList.remove("active");
     lightbox.children[0].removeAttribute("src");
   };
+
+  galleryItems.forEach((item) => item.onclick = () => lightboxShow(item));
+  accordionImgs.forEach((item) => item.onclick = () => lightboxShow(item));
   lightbox.onclick = () => lightBoxClose();
   window.onscroll = () => lightBoxClose();
+}
+
+/* ==================== ACCORDION ==================== */
+
+function accordion() {
+  const accordionBtns = document.querySelectorAll(".accordion__btn");
+  accordionBtns.forEach((accordion) => {
+    accordion.onclick = function () {
+      this.classList.toggle("active");
+      let content = this.nextElementSibling;
+      // accordion close
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+        this.children[1].innerHTML = "Click to reveal";
+      }
+      // accordion show
+      else {
+        content.style.maxHeight = content.scrollHeight + "px";
+        this.children[1].innerHTML = "Click to hide";
+      }
+    };
+  });
 }
