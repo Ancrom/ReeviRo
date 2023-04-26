99fce79f472734c1cc6fd4f25c004cf67d0821e9
Roman Antsupov <remon90@list.ru>
2023-04-26 06:20:28 +0300

    refactoring

--- js/index.js
+++ js/index.js
@@ -1,17 +1,16 @@
 /* ==================== BASE ==================== */
 
-const galleryItems = document.querySelectorAll(".gallery__item"); // using in changeAOSEffect and lightbox
 AOS.init(); // animate on scroll initialize
 changeAOSEffect(); // change AOS effect for some blocks depending on resolution
 window.onresize = () => changeAOSEffect();
-burger();
-lightbox();
-accordion();
+eventListener(); // lightbox, burger, accordion
 
 /* ==================== CHANGE AOS EFFECT ==================== */
 
 function changeAOSEffect() {
+  const galleryItems = document.querySelectorAll(".gallery__item");
   const nav = document.querySelector(".nav");
+
   if (window.innerWidth < 1026) nav.dataset.aos = "";
   if (window.innerWidth < 769) {
     galleryItems.forEach((item) => {
@@ -21,64 +20,68 @@ function changeAOSEffect() {
   }
 }
 
-/* ==================== BURGER ==================== */
+/* ==================== eventListener ==================== */
 
-function burger() {
-  const burger = document.querySelector(".burger");
+function eventListener() {
+  const lightbox = document.querySelector(".lightbox");
+  const lightboxImage = lightbox.querySelector("img");
   const nav = document.querySelector(".nav__menu");
-  // Burger show / close
-  burger.onclick = () => {
+  const burger = document.querySelector(".burger");
+  burger.prevent;
+
+  const toggleBurger = () => {
     burger.classList.toggle("active");
     nav.classList.toggle("active");
     document.body.classList.toggle("no-scroll");
   };
-}
 
-/* ==================== LIGHTBOX ==================== */
-
-function lightbox() {
-  const lightbox = document.querySelector(".lightbox");
-  const lightboxImage = lightbox.children[0];
-  const accordionImages = document.querySelectorAll(".accordion__img");
-  const lightboxShow = (item) => {
-    // changing scr to fullsize image
-    const image = item.children[0];
-    const src = image.getAttribute("src");
-    const newSrc = src.replace('/preview/', '/')
-		lightboxImage.setAttribute('src', newSrc)
-    lightbox.classList.add("active");
-    if (window.innerWidth < 768) document.body.classList.add("no-scroll");
-  };
   const lightBoxClose = () => {
     document.body.classList.remove("no-scroll");
     lightbox.classList.remove("active");
-    lightboxImage.removeAttribute("src");
+    lightboxImage.src = "";
+    window.onscroll = null;
   };
-  galleryItems.forEach((item) => (item.onclick = () => lightboxShow(item)));
-  accordionImages.forEach((item) => (item.onclick = () => lightboxShow(item)));
-  lightbox.onclick = () => lightBoxClose();
-  window.onscroll = () => lightBoxClose();
-}
 
-/* ==================== ACCORDION ==================== */
+  const lightboxShow = (target) => {
+    lightboxImage.src = target.src.replace("/preview/", "/");
+    lightbox.classList.add("active");
+    window.onscroll = lightBoxClose;
+    if (window.innerWidth < 768) document.body.classList.add("no-scroll");
+  };
 
-function accordion() {
-  const accordionBtns = document.querySelectorAll(".accordion__btn");
-  accordionBtns.forEach((item) => {
-    item.onclick = () => {
-      item.classList.toggle("active");
-      const content = item.nextElementSibling;
-      const btnText = item.children[1];
-      // accordion close
+  const toggleAccordion = (target) => {
+    const content = target.nextElementSibling;
+
+    if (target.classList.contains("accordion__btn")) {
+      target.classList.toggle("active");
+      const btnText = target.querySelector("span");
+
       if (content.style.maxHeight) {
         content.style.maxHeight = null;
         btnText.innerHTML = "Click to expand";
-      }
-      // accordion show
-      else {
+      } else {
         content.style.maxHeight = content.scrollHeight + "px";
         btnText.innerHTML = "Click to hide";
       }
-    };
-  });
+    }
+  };
+
+  const handleEvent = (e) => {
+    const target = e.target;
+
+    if (
+      target.classList.contains("gallery__image") ||
+      target.classList.contains("accordion__img")
+    ) {
+      lightboxShow(target);
+    } else if (target.classList.contains("lightbox")) {
+      lightBoxClose();
+    } else if (target.classList.contains("accordion__btn")) {
+      toggleAccordion(target);
+    } else if (target.classList.contains("burger")) {
+      toggleBurger();
+    }
+  };
+
+  window.addEventListener("click", handleEvent);
 }
